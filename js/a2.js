4ad9d603fbd2ce98bdd94d126ee1458b1f7b8313
han-96 <hangvu.96@outlook.com.vn>
2020-10-18 01:54:41 +0700

    Complete A2

--- js/a2.js
+++ js/a2.js
@@ -1,6 +1,6 @@
 //Test case:
 // nhập vào:
-[{
+teams = [{
     name: "Arsenal",
     points: 99,
     GD: 45
@@ -21,10 +21,36 @@
     GD: 39
 }]
 
-function swap(x, y) {
-    x = x + y;
-    y = x - y;
-    x = x - y;
-    return [x, y];
+// function swap(x, y) {
+//     var temp = x; 
+//     x = y;
+//     y = temp;
+//     return [x, y];
+// }
+
+// console.log(swap(3,2))
+
+for (let i = 0; i < teams.length-1; i++) {
+    for (let j = i + 1; j < teams.length; j++) {
+        if (teams[i]["points"] > teams[j]["points"]) {
+            teams[j] = [teams[i], teams[i] = teams[j]][0]
+        }
+        else if (teams[i]["points"] == teams[j]["points"]) {
+            if (teams[i]["GD"] > teams[j]["GD"]) {
+                teams[j] = [teams[i], teams[i] = teams[j]][0]
+            }
+            else if (teams[i]["GD"] == teams[j]["GD"]) {
+                if (teams[i]["name"][0] > teams[j]["name"][0]) {
+                    teams[j] = [teams[i], teams[i] = teams[j]][0]
+                }
+            }
+        }
+    }
+    
 }
 
+for (let i = 0; i < teams.length; i++) {
+    teams[i]["position"] = i
+}
+
+console.log(teams)
